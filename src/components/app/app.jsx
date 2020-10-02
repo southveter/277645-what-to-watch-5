af6af15812b7855a0ba464310995e2cfce83d71d
southveter <southveter@gmail.com>
2020-10-02 17:36:48 +0300

    Добавляет роутинг и новые компоненты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main";
+import SignIn from "../sign-in/sign-in";
+import MyList from "../my-list/my-list";
+import Film from "../film/film";
+import AddReview from "../add-review/add-review";
+import Player from "../player/player";
 
 const App = (props) => {
   const {title, genre, yearRelease} = props;
   return (
-    <Main
-      title={title}
-      genre={genre}
-      yearRelease={yearRelease}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main
+            title={title}
+            genre={genre}
+            yearRelease={yearRelease}
+          />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/mylist">
+          <MyList />
+        </Route>
+        <Route exact path="/films/:id">
+          <Film />
+        </Route>
+        <Route exact path="/films/:id/review">
+          <AddReview />
+        </Route>
+        <Route exact path="/player/:id">
+          <Player />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
