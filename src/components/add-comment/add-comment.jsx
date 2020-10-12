d1b319240dd0b5c759a3d6578a1e23bfb5b940c9
southveter <southveter@gmail.com>
2020-10-12 16:16:42 +0300

    Выполняет правки задания третьей лекции

--- src/components/add-comment/add-comment.jsx
+++ src/components/add-comment/add-comment.jsx
@@ -1,15 +1,11 @@
-import React, {PureComponent} from "react";
+import React from "react";
 
 class AddComment extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      star1: ``,
-      star2: ``,
-      star3: ``,
-      star4: ``,
-      star5: ``,
-      reviewtext: ``,
+      rating: ``,
+      [`review-text`]: ``,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -21,33 +17,33 @@ class AddComment extends React.PureComponent {
   }
 
   handleFieldChange(evt) {
-    const {id, value} = evt.target;
-    this.setState({[id]: value});
+    const {name, value} = evt.target;
+    this.setState({[name]: value});
   }
 
   render() {
     return <form onSubmit={this.handleSubmit} action="#" className="add-review__form">
       <div className="rating">
         <div className="rating__stars">
-          <input onChange={this.handleFieldChange} className="rating__input" id="star1" type="radio" name="rating" value="1" />
-          <label className="rating__label" htmlFor="star1">Rating 1</label>
+          <input onChange={this.handleFieldChange} className="rating__input" id="star-1" type="radio" name="rating" value="1" checked={this.state.name === this.value} />
+          <label className="rating__label" htmlFor="star-1">Rating 1</label>
 
-          <input onChange={this.handleFieldChange} className="rating__input" id="star2" type="radio" name="rating" value="2" />
-          <label className="rating__label" htmlFor="star2">Rating 2</label>
+          <input onChange={this.handleFieldChange} className="rating__input" id="star-2" type="radio" name="rating" value="2" checked={this.state.name === this.value} />
+          <label className="rating__label" htmlFor="star-2">Rating 2</label>
 
-          <input onChange={this.handleFieldChange} className="rating__input" id="star3" type="radio" name="rating" value="3" checked />
-          <label className="rating__label" htmlFor="star3">Rating 3</label>
+          <input onChange={this.handleFieldChange} className="rating__input" id="star-3" type="radio" name="rating" value="3" checked={this.state.name === this.value} />
+          <label className="rating__label" htmlFor="star-3">Rating 3</label>
 
-          <input onChange={this.handleFieldChange} className="rating__input" id="star4" type="radio" name="rating" value="4" />
-          <label className="rating__label" htmlFor="star4">Rating 4</label>
+          <input onChange={this.handleFieldChange} className="rating__input" id="star-4" type="radio" name="rating" value="4" checked={this.state.name === this.value} />
+          <label className="rating__label" htmlFor="star-4">Rating 4</label>
 
-          <input onChange={this.handleFieldChange} className="rating__input" id="star5" type="radio" name="rating" value="5" />
-          <label className="rating__label" htmlFor="star5">Rating 5</label>
+          <input onChange={this.handleFieldChange} className="rating__input" id="star-5" type="radio" name="rating" value="5" checked={this.state.name === this.value} />
+          <label className="rating__label" htmlFor="star-5">Rating 5</label>
         </div>
       </div>
 
       <div className="add-review__text">
-        <textarea onChange={this.handleFieldChange} className="add-review__textarea" name="review-text" id="reviewtext" placeholder="Review text"></textarea>
+        <textarea onChange={this.handleFieldChange} className="add-review__textarea" name="review-text" id="reviewtext" placeholder="Review text" value={this.state[`review-text`]}></textarea>
         <div className="add-review__submit">
           <button className="add-review__btn" type="submit">Post</button>
         </div>
